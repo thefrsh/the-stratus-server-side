60c65bef6764588a1d50acc6dde487b3cfb45308
Michal Broniewicz <mbroniewicz@outlook.com>
2020-10-16 01:40:55 +0200

    Initial commit

---
+++ src/test/java/io/github/thefrsh/stratus/ApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.thefrsh.stratus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
